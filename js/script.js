ac6b5fc7e4b06c185711f226c86bfb0dd40f7652
Benjamin <bennypythoncoder@gmail.com>
2023-08-24 08:52:15 +0200

    Fixed navbar menu not showing on index.html
    Fixed an issue where about-content came into view from the right, causing the navbar menu icon to be more to the right.

--- js/script.js
+++ js/script.js
@@ -47,8 +47,8 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'top' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'bottom' });
 
 
 /*==================== typed js ====================*/
